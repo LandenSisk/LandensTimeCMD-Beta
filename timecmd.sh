ff25172fc32eb5d1fc2e8f7b5b606779808475f8
Landen Sisk <58862240+LandenSisk@users.noreply.github.com>
2021-03-05 19:21:43 -0600

    The actual command

--- timecmd.sh
+++ timecmd.sh
@@ -1,7 +1,3 @@
-#Initializing the main variables
-thetime=$(date)
-#Using the main variables
-echo $thetime
 #Declaring our random message array
 array[0]="hello"
 array[1]="hi"
@@ -11,4 +7,22 @@ array[3]="hola"
 size=${#array[@]}
 index=$(($RANDOM % $size))
 #Using our new "random message" variables
+echo ${array[$index]}
+#Initializing the main variables
+thetime=$(date)
+#Using the main variables
+echo $thetime
+
+#Now im bullying you...
+tiparray[0]="When was the last time you've seen the sun?"
+tiparray[1]="You should read a book...just saying"
+tiparray[2]="Clean your room"
+tiparray[3]="You should talk a walk"
+tiparray[4]="Do you have any friends?"
+tiparray[5]="Go take a shower"
+tiparray[6]="A trip to the gym sounds fun"
+#Repeating the same process
+tipsize=${array[@]}
+tipindex=$(($RANDOM % $tipsize))
+#Get bullied nerd
 echo ${array[$index]}
