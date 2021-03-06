27a950e70cdf47c82bbd72e7a248d4eb23377921
Landen Sisk <58862240+LandenSisk@users.noreply.github.com>
2021-03-05 18:21:25 -0600

    The actual command

--- timecmd.sh
+++ timecmd.sh
@@ -1,11 +1,14 @@
+#Initializing the main variables
 thetime=$(date)
+#Using the main variables
 echo $thetime
-
+#Declaring our random message array
 array[0]="hello"
 array[1]="hi"
 array[2]="bonjour"
 array[3]="hola"
-
+#Declaring more variables for the random message array
 size=${#array[@]}
 index=$(($RANDOM % $size))
+#Using our new "random message" variables
 echo ${array[$index]}
